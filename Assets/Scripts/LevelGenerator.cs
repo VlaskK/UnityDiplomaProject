a792325a48fdb3f1d7908865c634ea7cdc444769
Charmink <mry2002@mail.ru>
2023-12-03 17:50:15 +0300

    Map generation

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public string levelFileName = "Assets/Scenes/level_1.txt";
+    public GameObject wallPrefab;
+    public GameObject floorPrefab;
+    public GameObject doorPrefab;
+    
+    public enum TileType
+    {
+        Wall,
+        Floor,
+        Door
+    }
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        string[] lines = File.ReadAllLines(levelFileName);
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            char[] chars = lines[y].ToCharArray();
+
+            for (int x = 0; x < chars.Length; x++)
+            {
+                Vector2 tilePosition = new Vector2(x, -y);
+
+                switch (chars[x])
+                {
+                    case '#':
+                        InstantiateTile(wallPrefab, tilePosition, TileType.Wall);
+                        break;
+
+                    case '.':
+                        InstantiateTile(floorPrefab, tilePosition, TileType.Floor);
+                        break;
+
+                    case 'D':
+                        InstantiateTile(doorPrefab, tilePosition, TileType.Door);
+                        break;
+
+                    // Другие символы могут быть добавлены в зависимости от ваших требований
+                }
+            }
+        }
+    }
+
+    void InstantiateTile(GameObject prefab, Vector2 position, TileType type)
+    {
+        GameObject tile = Instantiate(prefab, position, Quaternion.identity);
+        Tile tileData = new Tile();
+        tileData.type = type;
+        tileData.position = position;
+        tile.GetComponent<Tile>().type = tileData.type;
+        tile.GetComponent<Tile>().position = tileData.position;
+    }
+}
